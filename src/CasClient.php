0b00f26fa96c3fa4cbe10e130c45a80abaea60f2
王海洋 <93811139@qq.com>
2019-10-23 15:33:39 +0800

    bugfix error ticket check result

--- src/CasClient.php
+++ src/CasClient.php
@@ -1,12 +1,25 @@
 <?php
 namespace Yjtec\CasClient;
+
+use Exception;
 use YjtecCloud\Client\YjtecCloud;
-class CasClient{
-    public function check($ticket){
-        return YjtecCloud::rpc()
-            ->product('cas')
-            ->action('checkTicket')
-            ->options([$ticket])
-            ->request();
+
+class CasClient
+{
+    public function check($ticket)
+    {
+        try {
+            $return = YjtecCloud::rpc()
+                ->product('cas')
+                ->action('checkTicket')
+                ->options([$ticket])
+                ->request();
+        } catch (Exception $th) {
+        }
+        if (!isset($return['user'])) {
+            return false;
+        } else {
+            return $return;
+        }
     }
 }
